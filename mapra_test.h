c3b63de3e089600dbbc70d9dfe1624655fae9b64
fathrudinov <iskander.fakhrutdinov@rwth-aachen.de>
2022-05-29 21:57:55 +0200

    nur Vertauschung von Argumenten der Funktionen

--- mapra_test.h
+++ mapra_test.h
@@ -9,73 +9,84 @@
 #include <string>
 #include <vector>
 
-namespace mapra {
+namespace mapra
+{
 
-template <typename T>
-std::ostream& operator<<(std::ostream& stream, const std::vector<T>& vektor) {
-  stream << "(";
-  for (const auto& v : vektor) {
-    stream << v << ", ";
-  }
-  stream << ")";
-  return stream;
-}
-
-class MapraTest {
- public:
-  MapraTest(const std::string& case_name, std::ostream& out = std::cerr)
-      : name_(case_name), out_(out) {}
-  MapraTest(const MapraTest&) = delete;
-  MapraTest& operator=(const MapraTest&) = delete;
-  ~MapraTest() { Summary(); }
-
-  void Assert(const std::string& test_name, bool condition) {
-    ++num_tests_;
-    if (condition) {
-      out_ << "[   OK   ] Test '" << name_ << '.' << test_name << "' passed.\n";
-    } else {
-      out_ << "[ FAILED ] Test '" << name_ << '.' << test_name << "' failed.\n";
-      ++failed_tests_;
+  template <typename T>
+  std::ostream &operator<<(std::ostream &stream, const std::vector<T> &vektor)
+  {
+    stream << "(";
+    for (const auto &v : vektor)
+    {
+      stream << v << ", ";
     }
+    stream << ")";
+    return stream;
   }
 
-  template <typename T>
-  void AssertEq(const std::string& test_name, const T& expected_value,
-                const T& computed_value) {
-    const bool kPassed = expected_value == computed_value;
-    this->Assert(test_name, kPassed);
-    if (!kPassed)
-      out_ << "[        ] expected '" << expected_value << "', got '"
-           << computed_value << "'.\n";
-  }
+  class MapraTest
+  {
+  public:
+    MapraTest(const std::string &case_name, std::ostream &out = std::cerr)
+        : name_(case_name), out_(out) {}
+    MapraTest(const MapraTest &) = delete;
+    MapraTest &operator=(const MapraTest &) = delete;
+    ~MapraTest() { Summary(); }
 
-  template <typename T>
-  void AssertEq(const std::string& test_name,
-                const std::vector<T>& expected_value,
-                const std::vector<T>& computed_value) {
-    const bool kPassed = expected_value == computed_value;
-    this->Assert(test_name, kPassed);
-    if (!kPassed)
-      out_ << "[        ] expected '" << expected_value << "',\n"
-           << "[        ] got      '" << computed_value << "'.\n";
-  }
+    void Assert(const std::string &test_name, bool condition)
+    {
+      ++num_tests_;
+      if (condition)
+      {
+        out_ << "[   OK   ] Test '" << name_ << '.' << test_name << "' passed.\n";
+      }
+      else
+      {
+        out_ << "[ FAILED ] Test '" << name_ << '.' << test_name << "' failed.\n";
+        ++failed_tests_;
+      }
+    }
 
-  void Summary() {
-    out_ << "[========] Case '" << name_ << "': " << failed_tests_ << " out of "
-         << num_tests_ << " tests failed!\n";
-    if (failed_tests_)
-      out_ << "[ FAILED ] Go and fix some bugs!\n";
-    else
-      out_ << "[CONGRATS] Everything seems to be ok.\n";
-  }
+    template <typename T>
+    void AssertEq(const std::string &test_name, const T &expected_value,
+                  const T &computed_value)
+    {
+      const bool kPassed = expected_value == computed_value;
+      this->Assert(test_name, kPassed);
+      if (!kPassed)
+        out_ << "[        ] expected '" << expected_value << "', got '"
+             << computed_value << "'.\n";
+    }
 
- private:
-  int num_tests_ = 0;
-  int failed_tests_ = 0;
-  std::string name_;
-  std::ostream& out_ = std::cerr;
-};
+    template <typename T>
+    void AssertEq(const std::string &test_name,
+                  const std::vector<T> &computed_value,
+                  const std::vector<T> &expected_value)
+    {
+      const bool kPassed = expected_value == computed_value;
+      this->Assert(test_name, kPassed);
+      if (!kPassed)
+        out_ << "[        ] expected '" << expected_value << "',\n"
+             << "[        ] got      '" << computed_value << "'.\n";
+    }
 
-}  // namespace mapra
+    void Summary()
+    {
+      out_ << "[========] Case '" << name_ << "': " << failed_tests_ << " out of "
+           << num_tests_ << " tests failed!\n";
+      if (failed_tests_)
+        out_ << "[ FAILED ] Go and fix some bugs!\n";
+      else
+        out_ << "[CONGRATS] Everything seems to be ok.\n";
+    }
 
-#endif  // MAPRA_TEST_H_
+  private:
+    int num_tests_ = 0;
+    int failed_tests_ = 0;
+    std::string name_;
+    std::ostream &out_ = std::cerr;
+  };
+
+} // namespace mapra
+
+#endif // MAPRA_TEST_H_
